305dd306b3486223b38816ed5a6eae8722f0aaa5
BRIAN BARBOSA MARTINS <24122275@CFP127ALUNO.LOCAL>
2025-02-18 10:07:13 -0300

    okk

---
+++ dogapi.js
@@ -0,0 +1,38 @@
+'use scrict'
+
+
+async function pesquisarFotos(raca){
+    const url = `https://dog.ceo/api/breed/${raca}/images`
+   
+    const response = await fetch(url)
+    const data = await response.json()
+    
+    
+    return data.message 
+
+}
+
+function criarImagem(link) {
+    const galeria = document.getElementById('galeria')
+    const novaImg = document.createElement('img')
+    novaImg.src = link
+
+    galeria.appendChild(novaImg)
+}
+
+
+
+async function preencherFotos() {
+    const raca = document.getElementById('raca').value
+    const fotos = await pesquisarFotos(raca)
+    const galeria = document.getElementById('galeria')
+
+    galeria.replaceChildren('')
+  
+    
+    fotos.forEach(criarImagem)
+    console.log (fotos)
+}
+
+document.getElementById('pesquisar')
+        .addEventListener('click', preencherFotos)
